60a5c585bcc0fbeb2bbe0e2226f12b49be9fa76d
Apoorva Awasthy <apoorvaawasthy22@gmail.com>
2021-02-24 04:34:46 +0530

    Added error handlers

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const Destination = require("./models/destination");
 mongoose.connect("mongodb://localhost:27017/travelzila", {
   useNewUrlParser: true,
@@ -24,39 +26,65 @@ app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/destinations", async (req, res) => {
-  const destinations = await Destination.find({});
-  res.render("destinations/index", { destinations });
-});
-app.post("/destinations", async (req, res) => {
-  const destination = new Destination(req.body.destination);
-  await destination.save();
-  res.redirect(`/destinations/${destination._id}`);
-});
+app.get(
+  "/destinations",
+  catchAsync(async (req, res) => {
+    const destinations = await Destination.find({});
+    res.render("destinations/index", { destinations });
+  })
+);
+app.post(
+  "/destinations",
+  catchAsync(async (req, res, next) => {
+    const destination = new Destination(req.body.destination);
+    await destination.save();
+    res.redirect(`/destinations/${destination._id}`);
+  })
+);
 app.get("/destinations/new", (req, res) => {
   res.render("destinations/new");
 });
-app.get("/destinations/:id", async (req, res) => {
-  const destination = await Destination.findById(req.params.id);
-  res.render("destinations/show", { destination });
-});
-app.get("/destinations/:id/edit", async (req, res) => {
-  const destination = await Destination.findById(req.params.id);
-  res.render("destinations/edit", { destination });
-});
-app.put("/destinations/:id", async (req, res) => {
-  const { id } = req.params;
-  const destination = await Destination.findByIdAndUpdate(id, {
-    ...req.body.destination,
-  });
-  res.redirect(`/destinations/${destination._id}`);
-});
-app.delete("/destinations/:id", async (req, res) => {
-  const { id } = req.params;
-  await Destination.findByIdAndDelete(id);
-  res.redirect("/destinations");
+app.get(
+  "/destinations/:id",
+  catchAsync(async (req, res) => {
+    const destination = await Destination.findById(req.params.id);
+    res.render("destinations/show", { destination });
+  })
+);
+app.get(
+  "/destinations/:id/edit",
+  catchAsync(async (req, res) => {
+    const destination = await Destination.findById(req.params.id);
+    res.render("destinations/edit", { destination });
+  })
+);
+app.put(
+  "/destinations/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const destination = await Destination.findByIdAndUpdate(id, {
+      ...req.body.destination,
+    });
+    res.redirect(`/destinations/${destination._id}`);
+  })
+);
+app.delete(
+  "/destinations/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Destination.findByIdAndDelete(id);
+    res.redirect("/destinations");
+  })
+);
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = " Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
+});
 app.listen(3000, () => {
   console.log("Running on 3000");
 });
